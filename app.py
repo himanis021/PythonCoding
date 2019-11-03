1c04629b7f8a9a4fbb77c903ecaad5406f94f76b
HIMANI SHARMA <himanis021@gmail.com>
2019-11-04 01:11:43 +0530

    Basics to declare a variable and print operation

---
+++ app.py
@@ -0,0 +1,14 @@
+print("Hello New ")
+print('o----')
+print(' ||||')
+print('*' * 10)   # expression
+
+'''we check in a patient named John Smith. He's 20 years old and is a new client'''
+name = input('Whats your name? ')
+age = '20'
+is_new = True
+print("Patient's name: "+name+" age: "+age)
+
+
+
+
