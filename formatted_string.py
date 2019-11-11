24be01d10d12f24416cbce0b46f901541ea08c7c
HIMANI SHARMA <himanis021@gmail.com>
2019-11-12 00:38:04 +0530

    formatted strings and string methods

---
+++ formatted_string.py
@@ -0,0 +1,7 @@
+first = 'Himani'
+last = 'sharma'
+message = first + ' [' + last + '] is a coder'
+print(message)
+# formatted string
+msg = f'{first} [{last}] is the coder'
+print(msg)
---
+++ string_methods.py
@@ -0,0 +1,11 @@
+c = "Python for Beginners"
+print(len(c))
+print(c.upper())
+print(c.lower())
+print(c.title())
+print(c.find('p'))  # no small p so -1 output
+print(c.find('t'))
+print(c.find('for'))
+print(c.replace('py','Jy'))  # as there is Py not py
+print('python' in c)
+print('for' in c)
