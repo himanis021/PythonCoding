7eea6675a176333fe4fbd34b8dd25158b0af4b07
HIMANI SHARMA <himanis021@gmail.com>
2019-11-30 20:59:58 +0530

    Arithmetic operations and maths functions

---
+++ airthmetic_operations.py
@@ -0,0 +1,26 @@
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)   # shows output with exact precision of decimals
+print(11 // 3)   # shows only the integer and no integer
+print(10 % 3)   # modulus to show remainder value
+print(10 ** 3)   # 10 power to 3
+
+# augmented assignment code
+x = 10
+x += 3   # same as x=x+3
+print(x)
+
+'''
+order of precedence
+-parenthesis
+-exponentiation
+-multiplication or division
+-addition or subtraction
+'''
+
+x = 10*6/3+9-2**3
+print(x)
+
+xy = (2 + 3) * 10 - 3
+print(xy)
---
+++ math_functions.py
@@ -0,0 +1,10 @@
+import math    # python 3 math module so maths is an object so we can access its fucnitons or methods using dot.
+
+print(math.ceil(2.9))
+print(math.floor(2.9))
+
+x = 2.9
+print(round(x))
+print(abs(-2.9))   # always returns positive function
+
+# open python 3 maths functions
